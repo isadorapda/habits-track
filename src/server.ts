1e34a03693c73106890e1c12a472738540562b81
Isadora Pilau <isadorapda@gmail.com>
2023-01-19 14:32:10 -0300

    feat:created database table

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return ''
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
 })
 app.listen({
   port: 3333,
